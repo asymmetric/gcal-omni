c2bc7f33fc17bda98a86260672f6c1a3d5fc5d53
asymmetric <asymmetric@autistici.org>
2011-07-31 00:02:07 +0200

    implement all actions in sessions ctrl

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    redirect_to '/auth/google'
   end
 
   def create
-    render text: request.env['rack.auth'].inspect
+    auth = request.env['omniauth.auth']
+    user = User.find_by_uid(auth['uid']) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+
+    redirect_to root_url, notice: "Signed in!"
   end
 
   def destroy
     session[:user_id] = nil
+
     redirect_to root_url, notice: "Signed out!"
   end
 
   def failure
+    redirect_to root_url, alert: "Authentication error: #{params[:message].humanize}"
   end
 
 end
