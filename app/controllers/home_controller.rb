45a88837eff30f7ed969e15bde1d8bcdad51fbaa
asymmetric <asymmetric@autistici.org>
2011-07-31 00:49:50 +0200

    different home pages when logged-in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if logged_in?
+      render action: 'home'
+    else
+      render action: 'login'
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def session_bar
+    link_to 'Log out', logout_url if logged_in?
+  end
 end
