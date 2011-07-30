9944ce94c64271770702c6547211ddaf6b7a1c2b
asymmetric <asymmetric@autistici.org>
2011-07-30 21:36:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GcalOmni::Application.config.secret_token = '0b0939597bbc723768f5318cc3c620405f1947b4b89891459eb50e729f5a12937bae4bd0886b620d1822a76090e800d0e57663a779ebe6b4b876c261d4507f23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GcalOmni::Application.config.session_store :cookie_store, key: '_gcal-omni_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GcalOmni::Application.config.session_store :active_record_store
