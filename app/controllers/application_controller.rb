2b5fc273ce863c7a03c1f2c28b2d70f7bf703a49
asymmetric <asymmetric@autistici.org>
2011-07-31 01:49:49 +0200

    more helpers in app ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,22 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_logged_in
+    unless current_user
+      store_location
+      redirect_to root_url, error: "You must be logged-in to access this page"
+      return false
+    end
+  end
+
+  private
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def redirect_back_or_to_root
+    redirect_to(session[:return_to] || root_url)
+    session[:return_to] = nil
+  end
 end
